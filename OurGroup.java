abec9cefd7eeb729a1c5c5f20e3baaa1abff8fe2
korawan <korawan.ser@ku.th>
2021-02-08 13:50:49 +0700

    add name korawan to OutGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,8 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("YOUR NAME"); //PUT YOUR NAME HERE
+        groupMembers.add("Korawan"); //PUT YOUR NAME HERE
+
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
